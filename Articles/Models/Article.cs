8ad1828d043a5c32a69494cf7e9d385d3a75cc0e
Helena Miura <helenamiura@gmail.com>
2024-07-17 10:07:10 +0200

    files

---
+++ Articles/Models/Article.cs
@@ -0,0 +1,10 @@
+namespace Articles.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Contents { get; set; }
+    
+    
+}
